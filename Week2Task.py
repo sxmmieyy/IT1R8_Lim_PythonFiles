7ed43e2ea017f3fed0afd00f91e1628197a8966d
Sam Lim <officialsamacc@gmail.com>
2026-02-06 08:11:28 +0800

    Add files via upload

---
+++ Week2Task.py
@@ -0,0 +1,16 @@
+fullname = "Sam Adolph O. Lim";
+age = 18;
+height = 1.76;
+workstatus = False
+
+print ("Full Name:" , fullname)
+print(type(fullname))
+
+print ("Age:" , age)
+print(type(age))
+
+print ("Height in meters:" , height)
+print(type(height))
+
+print ("Currently Working?:" , workstatus)
+print(type(workstatus))
